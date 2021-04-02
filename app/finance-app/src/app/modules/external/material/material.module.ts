49c6b8a4a1ee8eba6f99fa42d7f3b8afaa6dd2e1
Jan Lewandowski <lewandowski.jan93@gmail.com>
2021-04-02 14:46:54 +0200

    feat: added date display format

--- app/finance-app/src/app/modules/external/material/material.module.ts
+++ app/finance-app/src/app/modules/external/material/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,7 +12,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatNativeDateModule } from '@angular/material/core';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -23,7 +23,20 @@ import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS, MomentDateAdapter, MomentDateModule } from '@angular/material-moment-adapter';
 import { ScrollingModule } from '@angular/cdk/scrolling';
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'LL',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 const modules: any[] = [
   MatInputModule,
@@ -50,13 +63,22 @@ const modules: any[] = [
   MatSlideToggleModule,
   MatSnackBarModule,
   MatAutocompleteModule,
-  MatDatepickerModule,
   MatNativeDateModule,
+  MatDatepickerModule,
+  MomentDateModule,
   ScrollingModule,
 ];
 
 @NgModule({
   imports: [...modules],
   exports: [...modules],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
 })
 export class MaterialModule {}
