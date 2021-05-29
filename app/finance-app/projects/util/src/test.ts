3b53d83702476a435a71849b79df96cfcc3d2d67
Jan Lewandowski <lewandowski.jan93@gmail.com>
2021-05-29 12:31:24 +0200

    fix(finace-app): #12

--- app/finance-app/projects/util/src/test.ts
+++ app/finance-app/projects/util/src/test.ts
@@ -1,8 +1,8 @@
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-import { getTestBed } from '@angular/core/testing';
-
 import 'zone.js';
 import 'zone.js/testing';
+
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { getTestBed } from '@angular/core/testing';
 
 declare const require: {
   context(
